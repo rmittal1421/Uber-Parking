f61fc6ecf0bcee065d39ee985d114d5b9134429c
Nipun Jindal <njindal@sfu.ca>
2019-06-25 21:39:26 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-import React from  'react'
-=======
 import React, {Component} from 'react'
 import {createStore, compose, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 import {rootReducer} from './redux/reducers'
 import Router from './Router'
->>>>>>> master
 
 class App extends Component {
     constructor(props) {
